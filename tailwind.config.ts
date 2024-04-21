3864e99c17f918d9183268313edceac58d3490f5
Adriel <adriel.fsantos@outlook.com>
2024-04-21 13:40:55 -0300

    feat: Add custom box shadows and colors to tailwind #1

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      boxShadow: {
+        button: '0 2px 4px rgba(0, 0, 0, 0.25)',
+        linkStar: '0 4px 4px rgba(0, 0, 0, 0.15)',
+      },
       fontSize: {
         heading: '2.625rem',
         subheading: '1.625rem',
@@ -18,6 +22,8 @@ const config: Config = {
       colors: {
         blackish: '#0f0f0f',
         primary: '#98d183',
+        disabledFg: '#666666',
+        disabledBg: '#d9d9d9',
       },
     },
   },
