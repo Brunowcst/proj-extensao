0c53429f0885c5db1088088746a8e689d7307d91
Adriel <adriel.fsantos@outlook.com>
2024-04-21 17:58:13 -0300

    feat: Update FormField type to use OptionProps interface #1

--- src/types/FormField.ts
+++ src/types/FormField.ts
@@ -1,14 +1,10 @@
-type Option = {
-  label: string;
-  value: string;
-  props?: object;
-};
+import { OptionProps } from 'react-select';
 
 export type FormField = {
   name: string;
   placeholder?: string;
   type?: string;
   as?: string;
-  options?: Option[];
+  options?: OptionProps[];
   props?: object;
 };
