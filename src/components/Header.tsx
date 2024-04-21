eda2c351f995721540c46ad1ff53cb6950783702
Adriel <adriel.fsantos@outlook.com>
2024-04-21 00:10:28 -0300

    feat: Improve Header layout #1

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,13 +8,13 @@ export default function Header() {
         width={360}
         height={258}
         alt=''
-        className='md:hidden'
+        className='md:hidden w-full'
       />
-      <div className='px-2 py-4'>
+      <div className='px-2 py-5'>
         <h1 className='text-subheading md:text-heading font-bold'>
           Aqui você é única!
         </h1>
-        <p className='text-lg md:text-subheading mb-2'>
+        <p className='text-lg md:text-subheading'>
           Insira os dados e reserve sua data agora!
         </p>
       </div>
