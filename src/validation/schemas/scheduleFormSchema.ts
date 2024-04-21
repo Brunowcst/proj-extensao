923c95b41617f56e2fe59be0f76851aad16c58ec
Adriel <adriel.fsantos@outlook.com>
2024-04-21 16:40:18 -0300

    feat: Add schedule form validation schema #1

---
+++ src/validation/schemas/scheduleFormSchema.ts
@@ -0,0 +1,23 @@
+import * as Yup from 'yup';
+import { validatePhone } from '../validators/phoneValidator';
+
+export const scheduleFormSchema = Yup.object().shape({
+  name: Yup.string()
+    .min(2, 'Curto demais, use no mínimo 2 caracteres')
+    .max(50, 'Longo demais, use no máximo 50 caracteres')
+    .required('Campo obrigatório'),
+  phone: Yup.string()
+    .required('Campo obrigatório')
+    .test({
+      test(value, context) {
+        const valiadtionResult = validatePhone(value);
+
+        if (valiadtionResult === '') return true;
+        return context.createError({ message: valiadtionResult });
+      },
+    }),
+  occasion: Yup.string().required('Campo obrigatório'),
+  date: Yup.date()
+    .required('Campo obrigatório')
+    .min(new Date(), 'Selecione uma data válida'),
+});
