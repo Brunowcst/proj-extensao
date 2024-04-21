35e977b2578c828ecc0caa7ec21672ab4f0081ca
Adriel <adriel.fsantos@outlook.com>
2024-04-21 00:10:45 -0300

    feat: Update Home page with InputField and Select components #1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,42 +1,56 @@
 import Header from '@/components/Header';
-import Input from '@/components/Input';
+import InputField from '@/components/InputField';
+import Select from '@/components/Select';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className='container mx-auto px-10'>
-        <Image src='/img/makeup.png' alt='' width='517' height='371' />
-        <form action=''>
-          <div>
-            <label htmlFor='name'>Nome:</label>
-            <Input id='name' name='name' type='text' required />
-          </div>
-          <div>
-            <label htmlFor='phone'>Telefone (WhatsApp):</label>
-            <Input id='phone' type='tel' name='phone' maxLength={15} required />
-          </div>
-          <div>
-            <label htmlFor='occasion'>Ocasião</label>
-            <select name='occasion' id='occasion' required>
-              <option value='' selected disabled>
-                Qual sua ocasião?
-              </option>
-              <option value='aniversario'>Aniversário</option>
-              <option value='batizado'>Batizado</option>
-              <option value='casamento'>Casamento</option>
-            </select>
-          </div>
-          <div>
-            <label htmlFor='date'>Data desejada para o atendimento</label>
-            <Input
-              id='date'
-              type='date'
-              required
-              min={new Date().toISOString().substring(0, 10)}
-            />
-          </div>
+      <main className='container mx-auto px-8 flex flex-col md:flex-row gap-16 justify-center'>
+        <Image
+          src='/img/makeup.png'
+          alt=''
+          width='517'
+          height='371'
+          className='hidden md:block rounded-4xl object-cover'
+        />
+        <form action='' className='mt-3 md:max-w-[50%]'>
+          <InputField
+            placeholder='Nome e sobrenome'
+            id='name'
+            name='name'
+            type='text'
+            required
+          />
+          <InputField
+            placeholder='Telefone (WhatsApp)'
+            id='phone'
+            name='phone'
+            type='tel'
+            required
+            maxLength={15}
+          />
+          <Select
+            name='occasion'
+            id='occasion'
+            required
+            label='Qual sua ocasião?'
+          >
+            <option value='' selected disabled>
+              Qual sua ocasião?
+            </option>
+            <option value='aniversario'>Aniversário</option>
+            <option value='batizado'>Batizado</option>
+            <option value='casamento'>Casamento</option>
+          </Select>
+          <InputField
+            placeholder='Data desejada para o atendimento'
+            id='date'
+            type='date'
+            required
+            min={new Date().toISOString().substring(0, 10)}
+          />
           <div>
             <button type='submit'>
               <Image src='/icons/star.svg' alt='' width='10' height='10' />
