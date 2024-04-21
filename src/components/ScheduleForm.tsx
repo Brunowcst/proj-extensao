03406064c598c07554a21cb94d0f861a0a075054
Adriel <adriel.fsantos@outlook.com>
2024-04-21 16:41:10 -0300

    feat: Update ScheduleForm with Formik components #1

--- src/components/ScheduleForm.tsx
+++ src/components/ScheduleForm.tsx
@@ -1,66 +1,18 @@
 'use client';
 
 import Button from '@/components/Button';
-import InputField from '@/components/InputField';
-import Select from '@/components/Select';
 import StarATag from '@/components/StarATag';
-import { useState } from 'react';
+import { scheduleForm } from '@/forms/scheduleForm';
+import FormikForm from './FormikForm';
 
 export default function ScheduleForm() {
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
-  const [occasion, setOccasion] = useState('');
-  const [date, setDate] = useState('');
-
   return (
-    <form
-      action=''
-      className='mt-3 md:max-w-[50%]'
-      onSubmit={(e) => e.preventDefault()}
+    <FormikForm
+      fields={scheduleForm.fields}
+      initialValues={scheduleForm.initialValues}
+      onSubmit={(values) => console.log(values)}
+      validationSchema={scheduleForm.validationSchema}
     >
-      <InputField
-        placeholder='Nome e sobrenome'
-        id='name'
-        name='name'
-        type='text'
-        required
-        value={name}
-        onChange={(e) => setName((e.target as HTMLInputElement).value)}
-      />
-      <InputField
-        placeholder='Telefone (WhatsApp)'
-        id='phone'
-        name='phone'
-        type='tel'
-        required
-        maxLength={15}
-        value={phone}
-        onChange={(e) => setPhone((e.target as HTMLInputElement).value)}
-      />
-      <Select
-        name='occasion'
-        id='occasion'
-        required
-        label='Qual sua ocasião?'
-        value={occasion}
-        onChange={(e) => setOccasion((e.target as HTMLSelectElement).value)}
-      >
-        <option value='' disabled>
-          Qual sua ocasião?
-        </option>
-        <option value='aniversario'>Aniversário</option>
-        <option value='batizado'>Batizado</option>
-        <option value='casamento'>Casamento</option>
-      </Select>
-      <InputField
-        placeholder='Data desejada para o atendimento'
-        id='date'
-        type='date'
-        required
-        min={new Date().toISOString().substring(0, 10)}
-        value={date}
-        onChange={(e) => setDate((e.target as HTMLInputElement).value)}
-      />
       <div className='flex flex-col md:flex-row justify-center items-center gap-3 md:gap-6 mt-6'>
         <Button type='submit' className='px-7 md:px-5 md:text-sm lg:text-base'>
           Agendar agora
@@ -69,6 +21,6 @@ export default function ScheduleForm() {
           Já fiz meu agendamento
         </StarATag>
       </div>
-    </form>
+    </FormikForm>
   );
 }
