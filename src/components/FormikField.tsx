ff8a69a30a17515f24daa54fdc4b46e77fe973a7
Adriel <adriel.fsantos@outlook.com>
2024-04-22 15:02:35 -0300

    fix: Remove console.log used for debugging #1

--- src/components/FormikField.tsx
+++ src/components/FormikField.tsx
@@ -6,26 +6,23 @@ interface IProps extends FieldAttributes<any> {}
 export default function FormikField({ className, mask, ...props }: IProps) {
   return (
     <Field name={props.name} {...props}>
-      {({ field, form, meta }: any) => {
-        console.log(form);
-        return (
-          <input
-            {...field}
-            placeholder={props.placeholder}
-            className={clsx(
-              'border border-blackish rounded-lg p-3 w-full focus:outline-primary caret-primary',
-              meta.error && meta.touched && 'border-red-600',
-              className
-            )}
-            onChange={(e) => {
-              if (mask) {
-                e.target.value = mask(e.target.value);
-              }
-              form.handleChange(e);
-            }}
-          />
-        );
-      }}
+      {({ field, form, meta }: any) => (
+        <input
+          {...field}
+          placeholder={props.placeholder}
+          className={clsx(
+            'border border-blackish rounded-lg p-3 w-full focus:outline-primary caret-primary',
+            meta.error && meta.touched && 'border-red-600',
+            className
+          )}
+          onChange={(e) => {
+            if (mask) {
+              e.target.value = mask(e.target.value);
+            }
+            form.handleChange(e);
+          }}
+        />
+      )}
     </Field>
   );
 }
