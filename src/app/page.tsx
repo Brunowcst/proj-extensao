d64ec73aaf8239cd7ae491ce8e7946d044f9d8f7
Brunowcst <brunoporfirio022@gmail.com>
2024-04-23 22:00:09 -0300

    send data function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
+'use client'
 import Image from "next/image";
+import { db } from "../../firebase/firebase";
+import { doc, setDoc, addDoc, collection } from "firebase/firestore";
+import { useState } from "react";
 
 export default function Home() {
+  const [data, setData] = useState("")
+
+  console.log(db);
+  
+
+  async function name() {
+    await addDoc(collection(db, "proj-exten"), {
+      name: "Teste",
+      phone: "(84) 99999-9999",
+      email: "teste123@gmail.com"
+    });
+  }  
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Teste</div>
+    <main>
+      <button onClick={name}>Enviar</button>
     </main>
   );
 }
